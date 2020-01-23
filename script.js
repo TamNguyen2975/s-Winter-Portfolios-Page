798922d50c57a32c94526b3f18c671a8a76ebe39
TamNguyen2975 <60240593+TamNguyen2975@users.noreply.github.com>
2020-01-23 13:56:44 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,17 @@
+$(".slide").click(function() {
+    $(".fff").hide();
+    $(".slide").text("Welcome To Our Class");
+    $(".slide").css("background-color", "red");
+}); 
+$(".slide").click(function() {
+    $(".fff").hide();
+    $(".slide").text("Welcome To Our Class");
+    $(".slide").css("background-color", "yellow");
+}); 
+$(".slide").click(function() {
+    $(".fff").hide();
+    $(".slide").text("Welcome To Our Class");
+    $(".slide").css("background-color", "yellow");
+});
+
+
